706b824ea6f27bdbafa66ea352ce21f44dfd6684
Dorian Suliński <doriansulinski07@gmail.com>
2025-04-22 22:41:43 +0200

    Create song.js

---
+++ song.js
@@ -0,0 +1,6 @@
+const songs = [
+    { title: "Song 1", artist: "Artist 1", src: "https://example.com/song1.mp3" },
+    { title: "Song 2", artist: "Artist 2", src: "https://example.com/song2.mp3" },
+    { title: "Song 3", artist: "Artist 3", src: "https://example.com/song3.mp3" },
+    { title: "Song 4", artist: "Artist 4", src: "https://example.com/song4.mp3" }
+];
